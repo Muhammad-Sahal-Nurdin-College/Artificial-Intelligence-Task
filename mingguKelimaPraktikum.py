9df8135fc86880a4eb5ee69695044ec0337263de
hp <sahalnurdin@gmail.com>
2022-04-02 16:04:35 +0700

    sip

--- mingguKelimaPraktikum.py
+++ mingguKelimaPraktikum.py
@@ -142,6 +142,7 @@ def main():
         # c = c + 1
         # ctr = ctr + 1
 """
+"""
 from __future__ import division
 from math import sqrt
 import random as rnd
@@ -225,4 +226,257 @@ while (1):
     elif checkGame(x, y) == 1:
         wins += 1
 
-    print('Wins:', wins, 'Losses:', losses, 'Ties:', ties)
+    print('Wins:', wins, 'Losses:', losses, 'Ties:', ties)"""
+"""
+import os
+import cv2
+import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import  ImageDataGenerator
+import tensorflow """
+
+import numpy as np
+import cv2
+import os
+import sys
+
+PATH = os.getcwd()+'\\'
+cap = cv2.VideoCapture(0)
+
+label = sys.argv[1]
+
+SAVE_PATH = os.path.join(PATH, label)
+
+try:
+    os.mkdir(SAVE_PATH)
+except FileExistsError:
+    pass
+
+ct = int(sys.argv[2])
+maxCt = int(sys.argv[3])+1
+print("Hit Space to Capture Image")
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Get Data : '+label,frame[50:350,150:450])
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        cv2.imwrite(SAVE_PATH+'\\'+label+'{}.jpg'.format(ct),frame[50:350,150:450])
+        print(SAVE_PATH+'\\'+label+'{}.jpg Captured'.format(ct))
+        ct+=1
+    if ct >= maxCt:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+"""""""
+from keras.models import model_from_json
+import numpy as np
+from skimage import io
+import cv2
+import random
+
+
+def prepImg(pth):
+    return cv2.resize(pth, (300, 300)).reshape(1, 300, 300, 3)
+
+
+def updateScore(play, bplay, p, b):
+    winRule = {'rock': 'scissor', 'scissor': 'paper', 'paper': 'rock'}
+    if play == bplay:
+        return p, b
+    elif bplay == winRule[play]:
+        return p + 1, b
+    else:
+        return p, b + 1
+
+
+with open('model.json', 'r') as f:
+    loaded_model_json = f.read()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("modelweights.h5")
+print("Loaded model from disk")
+
+shape_to_label = {'rock': np.array([1., 0., 0.]), 'paper': np.array([0., 1., 0.]), 'scissor': np.array([0., 0., 1.])}
+arr_to_shape = {np.argmax(shape_to_label[x]): x for x in shape_to_label.keys()}
+
+options = ['rock', 'paper', 'scissor']
+winRule = {'rock': 'scissor', 'scissor': 'paper', 'paper': 'rock'}
+rounds = 0
+botScore = 0
+playerScore = 0
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+loaded_model.predict(prepImg(frame[50:350, 100:400]))
+
+NUM_ROUNDS = 3
+bplay = ""
+
+while True:
+    ret, frame = cap.read()
+    frame = frame = cv2.putText(frame, "Press Space to start", (160, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0),
+                                2, cv2.LINE_AA)
+    cv2.imshow('Rock Paper Scissor', frame)
+    if cv2.waitKey(1) & 0xff == ord(' '):
+        break
+
+for rounds in range(NUM_ROUNDS):
+    pred = ""
+    for i in range(90):
+        ret, frame = cap.read()
+
+        # Countdown
+        if i // 20 < 3:
+            frame = cv2.putText(frame, str(i // 20 + 1), (320, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (250, 250, 0), 2,
+                                cv2.LINE_AA)
+
+        # Prediction
+        elif i / 20 < 3.5:
+            pred = arr_to_shape[np.argmax(loaded_model.predict(prepImg(frame[50:350, 100:400])))]
+
+        # Get Bots Move
+        elif i / 20 == 3.5:
+            bplay = random.choice(options)
+            print(pred, bplay)
+
+        # Update Score
+        elif i // 20 == 4:
+            playerScore, botScore = updateScore(pred, bplay, playerScore, botScore)
+            break
+
+        cv2.rectangle(frame, (100, 150), (300, 350), (255, 255, 255), 2)
+        frame = cv2.putText(frame, "Player : {}      Bot : {}".format(playerScore, botScore), (120, 400),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+        frame = cv2.putText(frame, pred, (150, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+        frame = cv2.putText(frame, "Bot Played : {}".format(bplay), (300, 140), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (250, 250, 0), 2, cv2.LINE_AA)
+        cv2.imshow('Rock Paper Scissor', frame)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+if playerScore > botScore:
+    winner = "You Won!!"
+elif playerScore == botScore:
+    winner = "Its a Tie"
+else:
+    winner = "Bot Won.."
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.putText(frame, winner, (230, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+    frame = cv2.putText(frame, "Press q to quit", (190, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2,
+                        cv2.LINE_AA)
+    frame = cv2.putText(frame, "Player : {}      Bot : {}".format(playerScore, botScore), (120, 400),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
+    cv2.imshow('Rock Paper Scissor', frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# train
+# -*- coding: utf-8 -*-
+"""Rock Paper Scissor GitHub
+Automatically generated by Colaboratory.
+Original file is located at
+    https://colab.research.google.com/drive/1bC3cfmlRCUSjd4OX1CHpuVvIeO6bE8_I
+"""
+
+import tensorflow.compat.v2 as tf
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from skimage import io
+import os
+import pickle
+import sys
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+"""Preparing our Data"""
+
+DATA_PATH = sys.argv[1] # Path to folder containing data
+
+shape_to_label = {'rock':np.array([1.,0.,0.,0.]),'paper':np.array([0.,1.,0.,0.]),'scissor':np.array([0.,0.,1.,0.]),'ok':np.array([0.,0.,0.,1.])}
+arr_to_shape = {np.argmax(shape_to_label[x]):x for x in shape_to_label.keys()}
+
+imgData = list()
+labels = list()
+
+for dr in os.listdir(DATA_PATH):
+    if dr not in ['rock','paper','scissor']:
+        continue
+    print(dr)
+    lb = shape_to_label[dr]
+    i = 0
+    for pic in os.listdir(os.path.join(DATA_PATH,dr)):
+        path = os.path.join(DATA_PATH,dr+'/'+pic)
+        img = cv2.imread(path)
+        imgData.append([img,lb])
+        imgData.append([cv2.flip(img, 1),lb]) #horizontally flipped image
+        imgData.append([cv2.resize(img[50:250,50:250],(300,300)),lb]) # zoom : crop in and resize
+        i+=3
+    print(i)
+
+np.random.shuffle(imgData)
+
+imgData,labels = zip(*imgData)
+
+imgData = np.array(imgData)
+labels = np.array(labels)
+
+"""Model"""
+
+from keras.models import Sequential,load_model
+from keras.layers import Dense,MaxPool2D,Dropout,Flatten,Conv2D,GlobalAveragePooling2D,Activation
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.optimizers import Adam
+from keras.applications.densenet import DenseNet121
+
+"""DenseNet"""
+
+densenet = DenseNet121(include_top=False, weights='imagenet', classes=3,input_shape=(300,300,3))
+densenet.trainable=True
+
+def genericModel(base):
+    model = Sequential()
+    model.add(base)
+    model.add(MaxPool2D())
+    model.add(Flatten())
+    model.add(Dense(3,activation='softmax'))
+    model.compile(optimizer=Adam(),loss='categorical_crossentropy',metrics=['acc'])
+    return model
+
+dnet = genericModel(densenet)
+
+checkpoint = ModelCheckpoint(
+    'model.h5',
+    monitor='val_acc',
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=True,
+    mode='auto'
+)
+
+es = EarlyStopping(patience = 3)
+
+history = dnet.fit(
+    x=imgData,
+    y=labels,
+    batch_size = 16,
+    epochs=8,
+    callbacks=[checkpoint,es],
+    validation_split=0.2
+)
+
+dnet.save_weights('model.h5')
+
+with open("model.json", "w") as json_file:
+    json_file.write(dnet.to_json())
